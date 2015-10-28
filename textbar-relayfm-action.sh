9f7f48955a20110a5837ac22d04367a136b1a724
TJ Luoma <luomat@gmail.com>
2015-10-27 20:10:13 -0400

    Initial Commit

---
+++ textbar-relayfm-action.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh -f
+# Actions to take after a TextBar Relay.fm menu bar item has been clicked
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2015-10-27
+
+NAME="$0:t:r"
+
+VERSION='0.0.1'
+
+
+exit 0
+#
+#EOF
+
---
+++ textbar-relayfm.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh -f
+# a Relay.fm menu bar item for TextBar
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2015-10-27
+
+NAME="$0:t:r"
+
+VERSION='0.0.1'
+
+
+
+exit 0
+#
+#EOF
+
+
