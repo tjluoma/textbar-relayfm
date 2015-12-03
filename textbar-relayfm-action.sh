4cf5518343e5474280cdd446b5d299acddc71174
TJ Luoma <luomat@gmail.com>
2015-12-02 19:52:54 -0500

    added Under the Radar and Less Than or Equal

--- textbar-relayfm-action.sh
+++ textbar-relayfm-action.sh
@@ -1,35 +1,208 @@
 #!/bin/zsh -f
-# Actions to take after a TextBar Relay.fm menu bar item has been clicked
+# open Relay Live
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
-# Date:	2015-10-27
+# Date:	2015-09-21
 
-if [ -e "$HOME/.path" ]
-then
-	source "$HOME/.path"
-else
-	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
-fi
+ 
 
 NAME="$0:t:r"
 
-VERSION='0.0.1'
 
-SELF_URL='https://raw.githubusercontent.com/tjluoma/textbar-relayfm/master/textbar-relayfm-action.sh'
+case "$TEXTBAR_TEXT" in
+	"Go to Relay FM Website")
+		open 'http://www.relay.fm'
 
-CURRENT_VERSION=`curl -sfL "$SELF_URL" | egrep '^VERSION='  | tr -dc '[0-9].'`
+		exit 0
+	;;
 
-if [ "$CURRENT_VERSION" != "$VERSION" ]
-then
-	echo "$NAME: Not up to date"
-else
-	echo "$NAME: Up To Date"
 
-fi
+	*Analog*)
+		open 'http://www.relay.fm/analogue'
+
+		exit 0
+	;;
+
+
+	*B-Sides*)
+		open 'http://www.relay.fm/b-sides'
+
+		exit 0
+	;;
+
+
+
+
+	*Bonanza*)
+		open 'http://www.relay.fm/bonanza'
+
+		exit 0
+	;;
+
+
+
+	*Clockwise*)
+		open 'http://www.relay.fm/clockwise'
+
+		exit 0
+	;;
+
+
+
+	*Connected*)
+		open 'http://www.relay.fm/connected'
+
+		exit 0
+	;;
+
+
+
+	*Cortex*)
+		open 'http://www.relay.fm/cortex'
+
+		exit 0
+	;;
+
+
+	*Inquisitive*)
+		open 'http://www.relay.fm/inquisitive'
+
+		exit 0
+	;;
+
+
+	*Isometric*)
+		open 'http://www.relay.fm/isometric'
+
+		exit 0
+	;;
+
+
+	*Less Than or Equal*)
+		open 'http://www.relay.fm/ltoe'
+
+		exit 0
+	;;
+
+	*Liftoff*)
+		open 'http://www.relay.fm/liftoff'
+
+		exit 0
+	;;
+
+
+	*Mac\ Power\ Users*|*MPU*)
+		open 'http://www.relay.fm/mpu'
+
+		exit 0
+	;;
+
+
+
+	*Material*)
+		open 'http://www.relay.fm/material'
+
+		exit 0
+	;;
+
+
+	*Reconcilable\ Differences*)
+		open 'http://www.relay.fm/rd'
+
+		exit 0
+	;;
+
+
+
+	*Rocket*)
+		open 'http://www.relay.fm/rocket'
+
+		exit 0
+	;;
+
+
+	*Pen\ Addict*)
+		open 'http://www.relay.fm/penaddict'
+
+		exit 0
+	;;
+
+
+	*Thoroughly\ Considered*)
+		open 'http://www.relay.fm/tc'
+
+		exit 0
+	;;
+
+
+	*Top\ Four*)
+		open 'http://www.relay.fm/topfour'
+
+		exit 0
+	;;
+
+	*Under the Radar*)
+		open 'http://www.relay.fm/radar'
+
+		exit 0
+	;;
+
+
+	*Upgrade*)
+		open 'http://www.relay.fm/upgrade'
+
+		exit 0
+	;;
+
+	*Virtual*)
+		open 'http://www.relay.fm/virtual'
+
+		exit 0
+	;;
+
+
+	*Chat\ Room*)
+		if [ -d '/Applications/Textual 5.app' ]
+		then
+			open -a 'Textual 5'
+		else
+			open 'http://webchat.freenode.net/?channels=relayfm&uio=d4'
+		fi
+
+	;;
+
+	"Listen Live in VLC")
+
+		
+		if [[ "`pgrep -x VLC`" == "" ]]
+		then
+				open    -a VLC 'http://amp.relay.fm:8000/stream'
+s		else
+				open -n -a VLC 'http://amp.relay.fm:8000/stream'
+		fi
+
+		exit 0
+	;;
+
+
+	"Listen Live"|"Listen Live in Browser")
+
+		open 'http://www.relay.fm/live'
+
+		exit 0
+	;;
+
+	"Currently Off The Air"|"View Schedule of Live Shows"|*)
+
+		open 'http://www.relay.fm/schedule'
+
+		exit 0
+	;;
+
+esac
 
 
 exit 0
 #
 #EOF
-
--- textbar-relayfm.sh
+++ textbar-relayfm.sh
@@ -1,15 +1,10 @@
 #!/bin/zsh -f
-# a Relay.fm menu bar item for TextBar
+# Purpose:
 #
-# From:	Timothy J. Luoma
+# From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
-# Date:	2015-10-27
-
-
-# @NOTDONE @TODO
-
-# Put a "Check for Updates" into menu
-# Put a "Check for Updates" script into "Actions"
+# Web: 	http://RhymesWithDiploma.com
+# Date:	2015-09-21
 
 if [ -e "$HOME/.path" ]
 then
@@ -20,44 +15,212 @@ fi
 
 NAME="$0:t:r"
 
-VERSION='0.0.1'
+PREVIOUS_LIVE_STATUS_FILE="$HOME/.$NAME.live.status.txt"
 
-SELF_URL='https://raw.githubusercontent.com/tjluoma/textbar-relayfm/master/textbar-relayfm.sh'
+[[ ! -e "$PREVIOUS_LIVE_STATUS_FILE" ]] && touch "$PREVIOUS_LIVE_STATUS_FILE"
 
-CURRENT_VERSION=`curl -sfL "$SELF_URL" | egrep '^VERSION=' | tr -dc '[0-9].'`
+TEMPFILE="${TMPDIR-/tmp}/${NAME}.${TIME}.$$.$RANDOM"
 
-echo "CURRENT_VERSION: $CURRENT_VERSION"
+curl -sfL 'http://www.relay.fm/live' > "${TEMPFILE}"
 
-if [ "$CURRENT_VERSION" != "$VERSION" -a "$CURRENT_VERSION" != "" ]
+if [[ ! -s "$TEMPFILE" ]]
 then
-	echo "$NAME: Not up to date"
+	# echo 'Relay Status Unknown'
+	sleep 5
+	exec "$0"
+	exit 0
+fi
 
-		# Make a temporary directory
-	TEMPDIR=`mktemp -q "${TMPDIR-/tmp}/${NAME}.XXXXXX`
+fgrep -iq '<h2>Not Currently Live</h2>' "$TEMPFILE"
 
-		# cd into temporary directory
-	cd "$TEMPDIR" || cd "$TMPDIR" || cd "/tmp"
+EXIT="$?"
 
-	FILENAME="$SELF_URL:t"
+if [ "$EXIT" = "0" ]
+then
 
-	rm -f "$FILENAME"
 
-	curl --silent --fail --location --remote-name "$SELF_URL"
+echo "
+Currently Off The Air
 
-	chmod 755 "$FILENAME"
+"
 
-	cp "$0" "$HOME/.Trash/$NAME.$VERSION.sh"
+####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
+#
+#		BEGIN - Get Next Show
+#
+
+	FILE="${TMPDIR-/tmp/}relayfm.$RANDOM.html"
+
+	PREVIOUS="${TMPDIR-/tmp/}relayfm.previous.html"
+
+	CURRENT_TIMEZONE=`date +%Z`
+
+	CALENDAR_URL_HTML="https://www.google.com/calendar/htmlembed?showTitle=0&showPrint=0&showTabs=0&showCalendars=0&mode=AGENDA&height=600&wkst=1&bgcolor=%23FFFFFF&src=relay.fm_t9pnsv6j91a3ra7o8l13cb9q3o@group.calendar.google.com&color=%23182C57&ctz=$CURRENT_TIMEZONE"
+
+	# get the whole HTML, and reformat it with `tidy` and save it to a local file we can parse
+	curl -sfL "$CALENDAR_URL_HTML" \
+	| tidy \
+		--char-encoding utf8 \
+		--wrap 0 \
+		--show-errors 0 \
+		--indent yes \
+		--input-xml no \
+		--output-xml no \
+		--quote-nbsp no \
+		--show-warnings no \
+		--uppercase-attributes no \
+		--uppercase-tags no \
+		--clean yes \
+		--force-output yes \
+		--join-classes yes \
+		--join-styles yes \
+		--markup yes \
+		--output-xhtml yes \
+		--quiet yes \
+		--quote-ampersand yes \
+		--quote-marks yes > "$FILE"
+
+	# if the file is not longer than 0 bytes, we didn't get the info we need
+	# so re-use the previous info
+	if [[ ! -s "$FILE" ]]
+	then
+
+		if [ -s "$PREVIOUS" ]
+		then
+			# if a previous file exists, use it
+			# take the first line
+			# but truncate it after 50 characters
+			# just in case it's full of junk
+			head -1 "$PREVIOUS" | colrm 50-
+
+			exit 0
+
+		else
+			# if a previous file doesn't exist
+			# or is 0 bytes, just give an error
+			echo "Relay FM Error"
+
+			exit 0
+		fi
+	fi
+
+	# look for the 'event-summary' as a span (important!) and then take the first one, and remove the HTML
+	SHOW_TITLE=`fgrep -A1 '<span class="event-summary">' "$FILE" | head -1 | sed 's#</span></a>##; s#.*>##g'`
+
+	# Make an ARRAY out of the date info
+	# we get something like
+	#           Fri May 1, 2015
+	# but we only _need_ the Month and DayOfMonth
+	# and strip any leading spaces or tabs
+	SHOW_DATE=($(fgrep -A1 '<div class="date">' "$FILE" | head -2 | tail -1 | sed 's#^[     ]*##g'))
+
+	# Get the time and strip any leading spaces or tabs
+	SHOW_TIME=`fgrep -A1 '<td class="event-time">' "$FILE" | head -2 | tail -1 | sed 's#^[     ]*##g'`
+
+	# This will give us the month as letters.
+	MONTH="${SHOW_DATE[2]}"
+
+	# Get the number, remove the comma
+	DAY_OF_MONTH=`echo "${SHOW_DATE[3]}" | tr -d ','`
+
+	# now get today's month/date (strip leading zeros from the day)
+	TODAYS_DATE=`date '+%b %d' | sed 's# 0*# #g'`
+
+	if [[ "$TODAYS_DATE" == "$MONTH $DAY_OF_MONTH" ]]
+	then
+		# The next show is today, just show time
+		echo "Next: $SHOW_TITLE ($SHOW_TIME)" | tee "$PREVIOUS"
+
+	else
+		# Output the show title, month/day @ time)
+		echo "Next: $SHOW_TITLE ($MONTH $DAY_OF_MONTH @ $SHOW_TIME)" | tee "$PREVIOUS"
+	fi
+
+
+
+######################## END - Get Next Show ######################## 
+#####################################################################  
+
+
+
+echo off > "$PREVIOUS_LIVE_STATUS_FILE"
+
+
+else
+
+	## There _IS_ a live show
+
+	SHOW_TITLE_RAW=`curl -sfL 'http://www.relay.fm/live' | fgrep -i '<title' `
+
+	SHOW_TITLE_CLEAN=`echo "${SHOW_TITLE_RAW}" | sed 's#<title>On-Air: ##g ; s# - Relay FM</title>##g'`
+
+	PREVIOUS_LIVE_STATUS=`head -1 "$PREVIOUS_LIVE_STATUS_FILE"`
+
+	if [[ "$PREVIOUS_LIVE_STATUS" != "live" ]]
+	then
+
+		if (( $+commands[growlnotify])) && (pgrep -qx Growl)
+		then
+			afplay /System/Library/Sounds/Glass.aiff >/dev/null
+
+			growlnotify \
+			--sticky \
+			--image "$HOME/Pictures/Icons/RelayFM/RelayFM-logo-growl.png" \
+			--identifier "$NAME" \
+			--message "Click to listen" \
+			--url "http://www.relay.fm/live" \
+			--title "$SHOW_TITLE_CLEAN is live"
+		fi
+
+		echo "live" > "$PREVIOUS_LIVE_STATUS_FILE"
+	fi
+
+	if [ -d '/Applications/VLC.app' ]
+	then
+		echo "$SHOW_TITLE_CLEAN\nListen Live in Browser\nListen Live in VLC\n---\nOpen Chat Room"
+	else
+		echo "$SHOW_TITLE_CLEAN\nListen Live\n---\nOpen Chat Room"
+	fi
+
 fi
 
 
-
-
-
-
-
+echo '
+View Schedule of Live Shows
+Go to Relay FM Website
+---
+Analog
+B-Sides
+Bonanza
+Clockwise
+Connected
+Cortex
+Inquisitive
+Isometric
+Less Than or Equal
+Liftoff
+Mac Power Users
+Material
+Reconcilable Differences
+Rocket
+Pen Addict
+Thoroughly Considered
+Top Four
+Under the Radar
+Upgrade
+Virtual
+'
 
 exit 0
+
+
+
+## This seems to 'work' even when it shouldn't. So that's no good.
+# http://amp.relay.fm:8000/stream
+# /usr/bin/nc -d -z amp.relay.fm 8000 2>&1 ) >/dev/null
+
+# curl -sfL --max-time 5 -o /dev/null http://amp.relay.fm:8000/stream
+# = exit code 28
+
 #
 #EOF
-
-
